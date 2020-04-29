5ef0014d3cfde430bd3a5cf645808f2865291cde
Julianrod <julianrod781@hotmail.com>
2020-04-29 13:57:42 -0500

    Updates

---
+++ precache-manifest.563f2a00e9fcd9ed53c3217b2cef5fd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05caccff45f5736a60e2fe5c145532f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "92b05a4fccf0e058f9b6",
+    "url": "/monster-rolodex/static/css/main.a841a624.chunk.css"
+  },
+  {
+    "revision": "684a63fdcfa1f79a6337",
+    "url": "/monster-rolodex/static/js/2.3ac13d0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.3ac13d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b05a4fccf0e058f9b6",
+    "url": "/monster-rolodex/static/js/main.e723ada1.chunk.js"
+  },
+  {
+    "revision": "b995881555f729ee0651",
+    "url": "/monster-rolodex/static/js/runtime-main.502946df.js"
+  }
+]);
